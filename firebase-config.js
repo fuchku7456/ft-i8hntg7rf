9114c4a0e3ca698c1076d2535bc125bddde63bcb
fuchku7456 <159561413+fuchku7456@users.noreply.github.com>
2024-02-09 08:01:30 -0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB14V0N0j9_MzQf7fBQSDeZ7CDvAjPxqkg",
+  authDomain: "hfe-ht6fdpk.firebaseapp.com",
+  databaseURL: "https://hfe-ht6fdpk-default-rtdb.firebaseio.com",
+  projectId: "hfe-ht6fdpk",
+  storageBucket: "hfe-ht6fdpk.appspot.com",
+  messagingSenderId: "873597667614",
+  appId: "1:873597667614:web:e661342b112e8fa1ca610f"
+};
